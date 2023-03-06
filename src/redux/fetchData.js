97a0d6a822a0afa27da9c075809e1f93fb5745a8
Milad Amiri <82150228+DigitalGenius-ui@users.noreply.github.com>
2023-03-06 13:49:39 +0800

    Update fetchData.js

--- src/redux/fetchData.js
+++ src/redux/fetchData.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const droneApi = createApi({
   reducerPath: 'droneApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://drone-api-production.up.railway.app/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://drone-api-production.up.railway.app' }),
   endpoints: (builder) => ({
     getAllSlides: builder.query({
       query: () => `slides`,
