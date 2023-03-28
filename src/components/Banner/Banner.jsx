e1506158321b2d23086b98368547ca4091e1ce6e
Milad Amiri <miladmarketing282.com>
2023-03-28 16:16:00 +0800

    added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Slider from 'react-slick';
+import React from "react";
+import Slider from "react-slick";
 // import { useGetAllSlidesQuery } from '../../redux/fetchData';
-import { settings } from '../slickSlider/SettingSlick';
-import SingleBanner from './SingleBanner';
+import { settings } from "../slickSlider/SettingSlick";
+import SingleBanner from "./SingleBanner";
 // import Skeleton from '@material-ui/lab/Skeleton';
-import styled from 'styled-components';
+import styled from "styled-components";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import datas from "../../Data/data.json";
@@ -12,12 +12,11 @@ import datas from "../../Data/data.json";
 const Banner = () => {
   // const { data, error, isLoading } = useGetAllSlidesQuery();
 
-  let productsItems = ""
-    productsItems = 
-    datas.slider.map((item, i) => (
-        <SingleBanner item={item} key={i}/>
-    ));
-    const slider = React.useRef(null);
+  let productsItems = "";
+  productsItems = datas.slider.map((item, i) => (
+    <SingleBanner item={item} key={i} />
+  ));
+  const slider = React.useRef(null);
   return (
     <>
       {/* {error && <div>something went wrong</div>}
@@ -37,15 +36,15 @@ const Banner = () => {
           {productsItems}
         </Slider>
         <button className="prev" onClick={() => slider?.current?.slickNext()}>
-          <ArrowForwardIosIcon />
+          <ArrowBackIosNewIcon />
         </button>
         <button className="next" onClick={() => slider?.current?.slickPrev()}>
-          <ArrowBackIosNewIcon />
+          <ArrowForwardIosIcon />
         </button>
       </Slides>
     </>
   );
-}
+};
 
 export default Banner;
 
@@ -92,48 +91,48 @@ export default Banner;
 //     }
 //   }
 //   .circle{
-//     width :230px; 
+//     width :230px;
 //     height : 230px;
 //     @media(max-width : 830px){
-//       width :200px; 
+//       width :200px;
 //       height : 200px;
 //     }
 //     @media(max-width : 670px){
-//       width :180px; 
+//       width :180px;
 //       height : 180px;
 //     }
 //     @media(max-width : 590px){
-//       width :150px; 
+//       width :150px;
 //       height : 150px;
 //     }
 //     @media(max-width : 510px){
-//       width :130px; 
+//       width :130px;
 //       height : 130px;
 //     }
 //     @media(max-width : 440px){
-//       width :100px; 
+//       width :100px;
 //       height : 100px;
 //     }
 //   }
 // `
 const Slides = styled.div`
-  position : relative;
-  .prev{
-    position : absolute;
-    top : 45%;
-    left : 3%;
+  position: relative;
+  .prev {
+    position: absolute;
+    top: 45%;
+    left: 3%;
     background-color: transparent;
-    cursor : pointer;
-    border : none;
+    cursor: pointer;
+    border: none;
     color: #05c4e6;
   }
-  .next{
-    position : absolute;
-    top : 45%;
-    right : 3%;
+  .next {
+    position: absolute;
+    top: 45%;
+    right: 3%;
     background-color: transparent;
-    cursor : pointer;
-    border : none;
+    cursor: pointer;
+    border: none;
     color: #05c4e6;
   }
-`
+`;
--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -38,20 +38,12 @@ const Category = () => {
               style={{ width: "3.7rem", opacity: "0.6" }}
               src={cat.logo}
               alt="signs"
-<<<<<<< HEAD
-              className={cat.title === "View All" ? "all" : ""}
+              className={cat.title === "View All" ? "all" : null}
             />
             <div>
               {cat.title}
               <ArrowRightIcon
-=======
-              className={cat.title === "View All" && "all"}
-            />
-            <div>
-              {cat.title}
-              <ArrowRight
->>>>>>> 287bb7b1a3f565e62e26da8dcd5c463b021e18d2
-                style={{ fontSize: "1.2rem", marginLeft: "-0.2rem" }}
+                className={cat.title === "View All" ? "all" : null}
               />
             </div>
           </Content>
